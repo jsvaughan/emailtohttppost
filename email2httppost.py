703c174d0064be8ef122fb1eb70d471bb6c0f749
Jon Vaughan <jsvaughan@gmail.com>
2012-05-02 19:55:13 +0100

    Config via yaml

--- email2httppost.py
+++ email2httppost.py
@@ -3,8 +3,9 @@ from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
+import os
 
-class LogSenderHandler(InboundMailHandler):
+class PostToUrl(InboundMailHandler):
     def receive(self, mail_message):
         logging.info("Received a message from: " + mail_message.sender)
         body = mail_message.bodies('text/plain').next()[1].decode()
@@ -14,11 +15,11 @@ class LogSenderHandler(InboundMailHandler):
             "email": mail_message.sender
         }
         form_data = urllib.urlencode(form_fields)
-        url = 'http://where.to.send.it'
-        result = urlfetch.fetch(url=url,
+
+        result = urlfetch.fetch(url=os.environ.get('DESTINATION_URL'),
             payload=form_data,
             method=urlfetch.POST,
             headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
 
-app = webapp.WSGIApplication([LogSenderHandler.mapping()], debug=True)
+app = webapp.WSGIApplication([PostToUrl.mapping()], debug=True)
